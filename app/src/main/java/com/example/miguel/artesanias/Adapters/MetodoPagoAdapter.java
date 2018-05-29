27ee09797fe82421a6d8136e4b2544684f432254
14030609 <mike.angyz@gmail.com>
2018-05-29 18:01:29 -0500

    proyecto

---
+++ app/src/main/java/com/example/miguel/artesanias/Adapters/MetodoPagoAdapter.java
@@ -0,0 +1,87 @@
+package com.example.miguel.artesanias.Adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.miguel.artesanias.Access;
+import com.example.miguel.artesanias.GeneraCompraActivity;
+import com.example.miguel.artesanias.Models.Cupones;
+import com.example.miguel.artesanias.Models.MetodoPago;
+import com.example.miguel.artesanias.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MetodoPagoAdapter extends RecyclerView.Adapter<MetodoPagoAdapter.MetodoPagoHolder> {
+
+    static int  id_metodopago;
+    static Cupones cupon;
+    Context context;
+    List<MetodoPago> metodosPago = new ArrayList<>();
+
+    public MetodoPagoAdapter(Context context, List<MetodoPago> metodoPagoList){
+        this.context = context;
+        this.metodosPago = metodoPagoList;
+    }
+
+    @Override
+    public MetodoPagoHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Toast.makeText(context,"ONcREATE", Toast.LENGTH_SHORT);
+        View view = LayoutInflater.from(context).inflate(R.layout.card_layout_metodo_pago, parent, false);
+        MetodoPagoHolder metodoPagoHolder = new MetodoPagoHolder(view);
+        return metodoPagoHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MetodoPagoHolder holder, int position) {
+        Toast.makeText(context,"ONbIEWVIEW", Toast.LENGTH_SHORT);
+        MetodoPago metodoPago = metodosPago.get(position);
+        holder.tvNombreMp.setText(metodoPago.getNombre());
+        holder.tvDescripcionMp.setText(metodoPago.getDecripcion());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return metodosPago.size();
+    }
+
+    class MetodoPagoHolder extends RecyclerView.ViewHolder{
+        TextView tvNombreMp, tvDescripcionMp;
+        public MetodoPagoHolder(View itemView) {
+            super(itemView);
+            tvNombreMp = (TextView) itemView.findViewById(R.id.tvNombreMp);
+            tvDescripcionMp = (TextView)itemView.findViewById(R.id.tvDescripMp);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    int position = getAdapterPosition();
+
+                    Snackbar.make(v, "Click detected on item " + position,
+                            Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    MetodoPago selectMP = metodosPago.get(position);
+                    cupon = Access.getCuponSelect();
+                    id_metodopago  = selectMP.getId_metodpago();
+                    Intent intent = new Intent(context, GeneraCompraActivity.class);
+                    context.startActivity(new Intent(intent));
+                }
+            });
+        }
+    }
+
+    public static int getIdPago()
+    { return id_metodopago;}
+
+    public static Cupones getCupon()
+    { return cupon;}
+}
---
+++ app/src/main/java/com/example/miguel/artesanias/perfilUsuario.java
@@ -0,0 +1,81 @@
+package com.example.miguel.artesanias;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class perfilUsuario extends AppCompatActivity {
+
+    private final String urlClient="http://"+Access.getDireccion()+"/api/bill/"+Access.getIdCliente();
+    private static TextView tvNombre, tvApellidos, tvRfc, tvTelefono, tvCorreo;
+    ListView lvDatosCliente;
+    String nombre;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_perfil_usuario);
+        tvNombre = (TextView) findViewById(R.id.textView_nombre);
+        tvApellidos = (TextView) findViewById(R.id.textView_apellido);
+        tvRfc = (TextView) findViewById(R.id.textView_rfc);
+        tvTelefono = (TextView) findViewById(R.id.text);
+        tvCorreo = (TextView) findViewById(R.id.textView_correo);
+        Toast.makeText(perfilUsuario.this, Access.getIdCliente(), Toast.LENGTH_LONG).show();
+        loadClientes();
+
+    }
+    private void loadClientes()
+    {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, urlClient, null,
+                new Response.Listener<JSONArray>(){
+
+                    public void onResponse(JSONArray response){
+                        Toast.makeText(perfilUsuario.this, response.toString(), Toast.LENGTH_SHORT).show();
+                        mostrarClient(response);
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        Toast.makeText(perfilUsuario.this, error.toString(), Toast.LENGTH_SHORT).show();
+                        error.printStackTrace();
+                    }
+                });
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    public  void mostrarClient (JSONArray response)
+    {
+        for (int i=0;i<response.length();i++)
+        {
+            try
+            {
+                JSONObject jsonObject =response.getJSONObject(i);
+                tvNombre.setText("Nombre :"+jsonObject.getString("name"));
+                tvApellidos.setText("Email :"+jsonObject.getString("email"));
+                //tvTelefono.setText("Teléfono :"+jsonObject.getString("address"));
+                tvRfc.setText("Ciudad :"+jsonObject.getString("city"));
+                tvCorreo.setText("Estado :"+jsonObject.getString("state"));
+            }
+            catch(JSONException e)
+            {
+                Toast.makeText(perfilUsuario.this, "Esta maaaaaal", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
